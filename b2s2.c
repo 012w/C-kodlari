bd0223d333f0ca53af24257f2da7dacd077486e7
012w <149322230+012w@users.noreply.github.com>
2025-07-22 21:56:35 +0300

    Create b2s2.c

---
+++ b2s2.c
@@ -0,0 +1,19 @@
+// kullanıcıdan 5 adet ondalıklı sayı alıp ardından ortalmasını alan kod
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float a, b, c, d, e;
+	printf("bes adet ondalikli sayi giriniz: \n" );
+	scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
+	
+	float toplam = a + b + c + d + e;
+	
+	float ortalama = toplam / 5;
+	
+	printf("Sonucumuz: %f\n", ortalama);
+	
+	return 0;
+}
