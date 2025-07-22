cf065c0d7b6a94eb44b971053fc9fc9a73307f2a
012w <149322230+012w@users.noreply.github.com>
2025-07-22 21:49:23 +0300

    Create b2s1.c

---
+++ b2s1.c
@@ -0,0 +1,30 @@
+// kullanıcının adını, soyadını, öğrenci numarasını ve ortalmasını alan, ardından bu girdileri ekranda çıktı veren uyuglama. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char isim[50]; 
+    char soyisim[50]; 
+    long long int ogrnum;
+    float ortalama;
+
+    printf("Adinizi Giriniz: \n"); 
+    scanf("%s", isim);
+
+    printf("Soyisminizi giriniz: \n");
+    scanf("%s", soyisim);
+
+    printf("Ogrenci Num. Giriniz: \n");
+    scanf("%d", &ogrnum);
+
+    printf("Ortalamanizi giriniz: \n");
+    scanf("%f", &ortalama);
+
+    printf("\nAdi: %s\n", isim);
+    printf("Soyisim: %s\n", soyisim);
+    printf("Ogrenci Numarasi: %d\n", ogrnum);
+    printf("Ogrenci Ortalamasi: %.2f\n", ortalama);
+
+    return 0;
+}
